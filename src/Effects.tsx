5f0685158c50c6f3bf3d71e68639fcaa304dc97a
Ilia_Petrovskii <iliaAPetrovskii@yandex.ru>
2022-11-28 16:13:24 +0300

    Рендеринг JSX

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,15 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useEffect, useState } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [data, setData] = useState<number>(-1);
+    useEffect(() => {
+        const func = (data: number) => setData(data);
+        subscribe(props.sourceId, func);
+        return () => {
+            unsubscribe(props.sourceId, func);
+            setData(-1);
+        };
+    }, [props.sourceId]);
+    return <div>{`${props.sourceId}: ${data}`}</div>;
 }
